00cf60b4dc0fa98ff2a0ece682f08dad056deb27
MasonMarc <masonmarcantel@gmail.com>
2022-12-21 17:23:08 -0500

    added support to write markdown to file and prompt to get name

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const prompt = inquirer.createPromptModule();
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  retucdrn `# ${data.title}
-`;
+const generateReadMe = (answers) => {
+  return `# ${answers.name}`;
 }
 
-module.exports = generateMarkdown;
+const writeToFile = (md, name) => {
+  try {
+      fs.writeFileSync('name.md', md);
+      console.log('Success!')    
+  } catch(err){
+      console.log(err);
+  }
+}
+
+prompt([
+  {
+      message: 'What is your name?',
+      name: 'name'
+  }
+  ])
+
+  .then(generateReadMe)
+.then(writeToFile)
+// module.exports = generateMarkdown;
