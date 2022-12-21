8ba4deb6d79f4e8175df4272539d541f52753b9f
MasonMarc <masonmarcantel@gmail.com>
2022-12-21 16:53:56 -0500

    added base code

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const static = require('./package.json');
+// TODO: Create an array of questions for user input
+const questions = [
+    "Question",
+    "Question",
+    "Question",
+    "Question",
+    "Question",
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
