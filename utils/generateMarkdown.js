88b52714741bd15f3e81b52f1d41f596d38a038f
MasonMarc <masonmarcantel@gmail.com>
2022-12-21 18:38:00 -0500

    added readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,71 +1,66 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const prompt = inquirer.createPromptModule();
 
 // TODO: Create a function to generate markdown for README
 const generateReadMe = (answers) => {
   return `# ${answers.title}
   
   # Descriptions
-
+  
   ${answers.description}
-
+  
   # Table of Contents
   
   [Descriptions](#descriptions)
   
   [Installation](#Installation)
-
+  
   [Usage](#Usage)
-
+  
   [License](#License)
   
   [Contributing](#Contributing)
-
+  
   [Tests](#Tests)
-
+  
   [Questions](#Questions)
   
-
+  
   # Installation
-
+  
   ${answers.install}
   
   # Usage
-
+  
   ${answers.usage}
-
+  
   # License
   
   # Contributing
-
+  
   ${answers.contribute}
   
   # Tests
-
+  
   ${answers.test}
-
-
+  
+  
   # Questions
-
+  
   For any questions, please reach out to me on GitHub at [${answers.github}](https://github.com/${answers.github}), or email me at ${answers.email}!
   `;
 }
 
-const writeToFile = (md, name) => {
-  try {
-      fs.writeFileSync('title.md', md);
-      console.log('Success!')    
-  } catch(err){
-      console.log(err);
-  }
+const writeToFile = (md) => {
+    fs.writeFileSync('readme.md', md);   
 }
 
+const prompt = inquirer.createPromptModule();
 prompt([
   {
-      message: 'What is the title of your project?',
-      name: 'title',
+    message: 'What is the title of your project?',
+    name: 'title',
   },
   {
     message: 'Please enter a description for your project',
@@ -97,5 +92,5 @@ prompt([
   }
   ])
 
-  .then(generateReadMe)
+.then(generateReadMe)
 .then(writeToFile)
